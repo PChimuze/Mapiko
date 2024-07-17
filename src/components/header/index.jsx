b633d88f5d18934a28d01c3fd1f01254f5f7b5ac
yurannhassengo <yurannhassengo@gmail.com>
2024-07-17 16:54:07 -0700

    Adding logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { NavBar } from './navBar'
 
 export const Header = () => {
+  
   return (
     <>
       <header className="flex items-center justify-between bg-black text-white h-20 px-8 text-lg shadow-lg border-b border-yellow-DEFAULT">
-        <div  className="text-2xl font-bold border border-yellow-DEFAULT p-2 rounded-md">Logo</div>
-        <NavBar/>
+        {/* Logo */}
+        <div className="flex items-center">
+          <img 
+            src="/logo.jpeg" 
+            alt="Logo" 
+            className="h-16 w-auto border border-yellow-DEFAULT rounded-md"
+          />
+        </div>
+        <NavBar />
       </header>
     </>
-  )
+  );
 }
 
 
