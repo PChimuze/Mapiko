ca1c28d336397ac1aa7aed83d891aca1ce32bf39
Badrú <sbsbadroo@gmail.com>
2024-07-30 14:18:51 +0200

    Pagina home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,17 +2,115 @@ import React from 'react'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 import { Outlet } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+import Bgvideo from '../components/media/o sucesso.mp4'
+import '../components/animations.css'
+
 
 export const Home = () => {
   return (
-    <>
+    <body className='bg-black transition-all duration-300 ease-in-out'>
+      
       <Header/>
-      <main>
-      Home
+      <motion.main
+        initial={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 0 }}
+      >
+         <video src={Bgvideo} autoPlay muted loop />
+            <div className="bg-overlay text-white ">
+                
+                <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2.0, delay: 0.2 }}
+            className="flex flex-col items-center justify-center pt-64"
+          >
+            <h1 className="text-4xl font-bold text-white mb-4">Bem-vindo ao Mapiko</h1>
+            <p className="text-lg text-white mb-8">Mensagem aleatória</p>
+
+
+            <motion.div  className='flex justify-between space-x-6'>
+
+            
+
+            <motion.div
+            initial={{ x: '100%', opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            >
+
+            <Link to="/portifolio">
+              <button className=" w-48 bg-blue-500/60 text-blue-50 px-6 py-3 rounded-xl hover:bg-blue-600 hover:text-white transition-colors duration-300">
+                Portifolio
+              </button>
+              </Link>
+              </motion.div>
+              <motion.div 
+initial={{ x: '-100%', opacity: 0 }}
+animate={{ x: 0, opacity: 1 }}
+transition={{ duration: 0.8, delay: 0.6 }}
+
+>
+
+<Link to="/servicos">
+  <button className=" w-48 bg-orange-400/60 text-blue-50 px-6 py-3 rounded-xl hover:bg-blue-600 hover:text-white transition-colors duration-300">
+  Serviços
+  </button>
+</Link>
+</motion.div>
+
+
+
+              </motion.div>
+
+
+
+           
+          </motion.div> 
+
+          <motion.div  className='flex justify-center mt-4 space-x-6'>
+
+            
+
+
+
+<motion.div 
+ initial={{ x: '-100%', opacity: 0 }}
+ animate={{ x: 0, opacity: 1 }}
+ transition={{ duration: 0.8, delay: 0.6 }}
+           
+>
+           
+            <Link to="/contactos">
+              <button className=" w-48 bg-black/60 text-blue-50 px-6 py-3 rounded-xl hover:bg-blue-600 hover:text-white transition-colors duration-300">
+              Entrar em contacto
+              </button>
+            </Link>
+            </motion.div>
+
+
+<motion.div
+initial={{ x: '100%', opacity: 0 }}
+animate={{ x: 0, opacity: 1 }}
+transition={{ duration: 0.8, delay: 0.6 }}
+>
+
+<Link to="/sobre-nos">
+  <button className=" w-48 bg-black/60 text-blue-50 px-6 py-3 rounded-xl hover:bg-blue-600 hover:text-white transition-colors duration-300">
+    Sobre Nós
+      </button>
+  </Link>
+  </motion.div>
+  </motion.div>
+  </div>
+  
+
       <Outlet/>
-      </main>
+      </motion.main>
       <Footer/>
-    </>
+      </body>
   )
 
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { NavBar } from './navBar'
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   
   return (
     <>
-      <header className="flex items-center justify-between bg-black text-white h-20 px-8 text-lg shadow-lg border-b border-yellow-DEFAULT">
+      <header className="flex items-center justify-between bg-black text-white h-16 px-8 text-lg shadow-lg ">
         {/* Logo */}
         <div className="flex items-center">
+          <Link to={"/"}>
           <img 
             src="/logo.jpeg" 
             alt="Logo" 
-            className="h-16 w-auto border border-yellow-DEFAULT rounded-md"
-          />
+            className="h-12 w-auto border border-yellow-DEFAULT rounded-md"
+            />
+            </Link>
         </div>
         <NavBar />
       </header>
